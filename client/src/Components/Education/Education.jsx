2f1a0a7c60c933120f77a1573608c0fbc8ef5c6d
Dominic Monares <78285106+DominicMonares@users.noreply.github.com>
2022-05-05 03:06:35 -0700

    complete education styling

--- client/src/Components/Education/Education.jsx
+++ client/src/Components/Education/Education.jsx
@@ -5,25 +5,36 @@ const Education = () => {
     <div className="education_container">
       <div className="edu_container">
         <div className="edu_header">
-          <span className="edu_cert">
-            Certificate, Advanced Software Engineering Immersive
-          </span>
-          <a className="edu_est" href="https://www.hackreactor.com/">Hack Reactor</a>
-          <span className="edu_location">US Remote</span>
+          <div className="edu_left">
+            <span className="edu_cert">
+              Certificate, Advanced Software Engineering Immersive
+            </span>
+            &nbsp;|&nbsp;
+            <a className="edu_link" href="https://www.hackreactor.com/">Hack Reactor</a>
+            &nbsp;|&nbsp;
+            <span className="edu_location">US Remote</span>
+          </div>
           <span className="edu_year">2021 - 2022</span>
         </div>
-        <span className="edu_bullet">
-          • 36-week software engineering immersive program with over 1000 hours of coding, full-stack development with JavaScript, React, Express, MongoDB, and MySQL.
+        <span className="edu_bullet_container">
+          <p className="bullet">•</p>
+          <p className="bullet_text">
+            36-week software engineering immersive program with over 1000 hours of coding, full-stack development with JavaScript, React, Express, MongoDB, and MySQL.
+          </p>
         </span>
       </div>
       <div className="edu_container">
         <div className="edu_header">
-          <span className="edu_cert">
-            High School Diploma
-          </span>
-          <span className="edu_est">Oceana High School</span>
-          <span className="edu_location">US Remote</span>
-          <span className="edu_year">2021 - 2022</span>
+          <div className="edu_left">
+            <span className="edu_cert">
+              High School Diploma
+            </span>
+            &nbsp;|&nbsp;
+            <span className="edu_est">Oceana High School</span>
+            &nbsp;|&nbsp;
+            <span className="edu_location">Pacifica, CA</span>
+          </div>
+          <span className="edu_year">2009 - 2013</span>
         </div>
       </div>
     </div>
