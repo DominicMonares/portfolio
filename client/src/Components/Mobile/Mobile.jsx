37f78feaa0db706540a00e4c34bca503e807fcaf
Dominic Monares <78285106+DominicMonares@users.noreply.github.com>
2022-05-09 23:09:03 -0700

    add mobile

---
+++ client/src/Components/Mobile/Mobile.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Mobile = () => {
+  return (
+    <div className="mobile_container">
+      <span className="mobile_text">Mobile browsers are currently unsupported {':('}</span>
+    </div>
+  );
+}
+
+export default Mobile;
