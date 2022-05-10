0566f6d52657d102c9362a580aa5ed249f7b1379
Dominic Monares <78285106+DominicMonares@users.noreply.github.com>
2022-05-09 21:18:00 -0700

    change port

--- server/index.js
+++ server/index.js
@@ -2,5 +2,5 @@ const createServer = require('./server.js');
 
 const app = createServer();
 
-app.listen(3000, () => { console.log('Listening on port 3000') });
+app.listen(80, () => { console.log('Listening on port 80') });
 
