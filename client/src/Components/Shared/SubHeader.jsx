328fba2d50864c67298e4ac47a0dc5ea7f6d9da4
Dominic Monares <78285106+DominicMonares@users.noreply.github.com>
2022-09-24 19:37:41 -0700

    init

---
+++ client/src/Components/Shared/SubHeader.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const SubHeader = ({ desc, links }) => {
+  return (
+    <div className="app_header_container">
+      <span>{desc}</span>
+      &nbsp;|&nbsp;
+      {links.map((link, index) => {
+        const notLast = index < links.length - 1;
+        return (
+          <>
+            <a
+              className="exp_link"
+              href={link.link}
+              target="_blank"
+              key={link.link}
+            >
+              {link.title}
+            </a>
+            {notLast ? <>&nbsp;|&nbsp;</> : <></>}
+          </>
+        );
+      })}
+    </div>
+  );
+}
+
+export default SubHeader;
