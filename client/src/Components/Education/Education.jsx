c89e7f43546d04e775e2bfe495c437d7076dfa26
Dominic Monares <78285106+DominicMonares@users.noreply.github.com>
2022-09-24 17:09:42 -0700

    refactor education

--- client/src/Components/Education/Education.jsx
+++ client/src/Components/Education/Education.jsx
@@ -1,48 +1,26 @@
 import React from 'react';
 
+import Header from '../Shared/Header';
+import Bullets from '../Shared/Bullets';
+import education from '../../../../data/education';
+
 const Education = () => {
   return (
     <div className="education_container">
-      <div className="edu_container">
-        <div className="edu_header_container">
-          <div>
-            <span className="edu_cert">
-              Certificate, Advanced Software Engineering Immersive
-            </span>
-            &nbsp;|&nbsp;
-            <a
-              className="edu_link"
-              href="https://www.hackreactor.com/"
-              target="_blank"
-            >
-              Hack Reactor
-            </a>
-            &nbsp;|&nbsp;
-            <span className="edu_location">US Remote</span>
+      {education.map(edu => {
+        return (
+          <div className="edu_container">
+            <Header
+              title={edu.title}
+              est={edu.est}
+              estLink={edu.estLink}
+              location={edu.location}
+              year={edu.year}
+            />
+            {edu.bullets ? <Bullets bullets={edu.bullets} /> : <></>}
           </div>
-          <span className="edu_year">2021 - 2022</span>
-        </div>
-        <span className="edu_bullet_container">
-          •
-          <p className="edu_bullet_text">
-            36-week software engineering immersive program with over 1000 hours of coding, full-stack development with JavaScript, React, Express, MongoDB, and MySQL.
-          </p>
-        </span>
-      </div>
-      <div className="edu_container">
-        <div className="edu_header_container">
-          <div>
-            <span className="edu_cert">
-              High School Diploma
-            </span>
-            &nbsp;|&nbsp;
-            <span className="edu_est">Oceana High School</span>
-            &nbsp;|&nbsp;
-            <span className="edu_location">Pacifica, CA</span>
-          </div>
-          <span className="edu_year">2009 - 2013</span>
-        </div>
-      </div>
+        );
+      })}
     </div>
   );
 }
