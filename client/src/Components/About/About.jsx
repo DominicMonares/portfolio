fb22ac5c71ba911fc081cd753a8422738597f30c
Dominic Monares <78285106+DominicMonares@users.noreply.github.com>
2022-10-01 21:46:43 -0700

    save icon progress

--- client/src/Components/About/About.jsx
+++ client/src/Components/About/About.jsx
@@ -8,21 +8,19 @@ import './About.css';
 
 const About = () => {
   return (
-    <>
-      <div className="about_body_container">
-        <div className="about_body">
-          <div>
-            <img className="profile_pic" src={profile_pic} width="300px" height="300px" />
-          </div>
+    <div className="about_container">
+      <div>
+        <div className="about_body_container">
+          <img className="profile_pic" src={profile_pic} width="300px" height="300px" />
           <div className="about_text_container">
             {about.map(p => <p className="about_text">{p}</p>)}
           </div>
         </div>
+        <div className="about_container_underline"></div>
+        <Contacts />
       </div>
-      <div className="about_container_underline"></div>
-      <Contacts />
       <Icons page="About" />
-    </>
+    </div>
   );
 }
 
