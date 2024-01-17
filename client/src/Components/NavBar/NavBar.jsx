73851f53be08742b9b9bb0d50119a85eaf59d965
DominicMonares <dominicsmonares@gmail.com>
2024-01-16 21:08:56 -0800

    save progress

--- client/src/Components/NavBar/NavBar.jsx
+++ client/src/Components/NavBar/NavBar.jsx
@@ -8,72 +8,64 @@ const NavBar = () => {
   const location = useLocation();
   const tabs = {};
   navigation.forEach(p => {
-    p.route === location.pathname ? tabs[p.page] = 'nav-tab-active' : tabs[p.page] = 'nav-tab';
+    p.route === location.pathname ? tabs[p.page] = '-active' : tabs[p.page] = '';
   });
 
-  const [activeTab, setActiveTab] = useState(tabs);
+  const [active, setActive] = useState(tabs);
 
   const updateTabs = (tab) => {
     const currentTabs = { ...tabs };
     for (const t in currentTabs) {
       if (t === tab) {
-        currentTabs[t] = 'nav-tab-active';
+        currentTabs[t] = '-active';
       } else {
-        currentTabs[t] = 'nav-tab';
+        currentTabs[t] = '';
       }
     }
 
-    setActiveTab(currentTabs);
+    setActive(currentTabs);
   }
 
   return (
     <div className='nav-container'>
       <div className='nav-menu'>
-        <span className=''>
-          <NavLink
-            to={'/apps'}
-            className={activeTab['Apps']}
-            onClick={() => updateTabs('Apps')}
-          >
-            <div className='nav-text'>Applications</div>
-          </NavLink>
-        </span>
-        <span className=''>
-          <NavLink
-            to={'/skills'}
-            className={activeTab['Skills']}
-            onClick={() => updateTabs('Skills')}
-          >
-            <div className='nav-text'>Skills</div>
-          </NavLink>
-        </span>
-        <span className=''>
-          <NavLink
-            to={'/experience'}
-            className={activeTab['Experience']}
-            onClick={() => updateTabs('Experience')}
-          >
-            <div className='nav-text'>Experience</div>
-          </NavLink>
-        </span>
-        <span className=''>
-          <NavLink
-            to={'/education'}
-            className={activeTab['Education']}
-            onClick={() => updateTabs('Education')}
-          >
-            <div className='nav-text'>Education</div>
-          </NavLink>
-        </span>
-        <span className=''>
-          <NavLink
-            to={'/about'}
-            className={activeTab['About']}
-            onClick={() => updateTabs('About')}
-          >
-            <div className='nav-text'>About</div>
-          </NavLink>
-        </span>
+        <span className="tab-curve tab-1-curve-left"></span>
+        <span className={'tab-square'.concat(' tab-1-square-left', active['Apps'])}></span>
+        <NavLink
+          to={'/apps'}
+          className={'nav-tab'.concat(active['Apps'], ' tab-1')}
+          onClick={() => updateTabs('Apps')}
+        >
+          <div className='nav-text'>Applications</div>
+        </NavLink>
+        <NavLink
+          to={'/skills'}
+          className={'nav-tab'.concat(active['Skills'], ' tab-2')}
+          onClick={() => updateTabs('Skills')}
+        >
+          <div className='nav-text'>Skills</div>
+        </NavLink>
+        <NavLink
+          to={'/experience'}
+          className={'nav-tab'.concat(active['Experience'], ' tab-3')}
+          onClick={() => updateTabs('Experience')}
+        >
+          <div className='nav-text'>Experience</div>
+        </NavLink>
+        <NavLink
+          to={'/education'}
+          className={'nav-tab'.concat(active['Education'], ' tab-4')}
+          onClick={() => updateTabs('Education')}
+        >
+          <div className='nav-text'>Education</div>
+        </NavLink>
+        <NavLink
+          to={'/about'}
+          className={'nav-tab'.concat(active['About'], ' tab-5')}
+          onClick={() => updateTabs('About')}
+        >
+          <div className='nav-text'>About</div>
+        </NavLink>
       </div>
       <div className='header-underline'></div>
     </div>
