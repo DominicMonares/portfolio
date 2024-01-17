8645e0063d105cebfac6a26ebde380f6de74e768
DominicMonares <dominicsmonares@gmail.com>
2024-01-16 19:15:53 -0800

    add missing tabs

--- client/src/Components/NavBar/NavBar.jsx
+++ client/src/Components/NavBar/NavBar.jsx
@@ -30,13 +30,48 @@ const NavBar = () => {
     <div className='nav-container'>
       <div className='nav-menu'>
         <span className=''>
-          <NavLink to={'/apps'} className={activeTab['Apps']} onClick={() => updateTabs('Apps')}>
+          <NavLink
+            to={'/apps'}
+            className={activeTab['Apps']}
+            onClick={() => updateTabs('Apps')}
+          >
             <div className='nav-text'>Applications</div>
           </NavLink>
         </span>
         <span className=''>
-          <NavLink to={'/skills'} className={activeTab['Skills']} onClick={() => updateTabs('Skills')}>
+          <NavLink
+            to={'/skills'}
+            className={activeTab['Skills']}
+            onClick={() => updateTabs('Skills')}
+          >
             <div className='nav-text'>Skills</div>
+          </NavLink>
+        </span>
+        <span className=''>
+          <NavLink
+            to={'/experience'}
+            className={activeTab['Experience']}
+            onClick={() => updateTabs('Experience')}
+          >
+            <div className='nav-text'>Experience</div>
+          </NavLink>
+        </span>
+        <span className=''>
+          <NavLink
+            to={'/education'}
+            className={activeTab['Education']}
+            onClick={() => updateTabs('Education')}
+          >
+            <div className='nav-text'>Education</div>
+          </NavLink>
+        </span>
+        <span className=''>
+          <NavLink
+            to={'/about'}
+            className={activeTab['About']}
+            onClick={() => updateTabs('About')}
+          >
+            <div className='nav-text'>About</div>
           </NavLink>
         </span>
       </div>
