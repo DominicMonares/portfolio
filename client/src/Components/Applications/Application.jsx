9de40c8ba357be690e9a522a50e1a6e122476b46
DominicMonares <dominicsmonares@gmail.com>
2024-01-20 20:00:24 -0800

    save progress

--- client/src/Components/Applications/Application.jsx
+++ client/src/Components/Applications/Application.jsx
@@ -29,6 +29,15 @@ const Application = ({ wide, app }) => {
     if (currentSlide !== index) setCurrentSlide(index)
   }
 
+  const arrowStyles = {
+    position: 'absolute',
+    zIndex: 2,
+    top: 'calc(50% - 15px)',
+    width: 30,
+    height: 30,
+    cursor: 'pointer',
+  };
+
   return (
     <div className={wide ? 'w_app_container' : 'm_app_container'}>
       <Header
@@ -45,23 +54,45 @@ const Application = ({ wide, app }) => {
         interval={6000}
         onChange={updateCurrentSlide}
         selectedItem={currentSlide}
-        showArrows={false}
         showIndicators={false}
         showStatus={false}
-        width="95%"
+        renderArrowPrev={(onClickHandler, hasPrev, label) =>
+          hasPrev && (
+            <button
+              type="button"
+              onClick={onClickHandler}
+              title={label}
+              style={{ ...arrowStyles, left: 15 }}
+            >
+              {'<'}
+            </button>
+          )
+        }
+        renderArrowNext={(onClickHandler, hasNext, label) =>
+          hasNext && (
+            <button
+              type="button"
+              onClick={onClickHandler}
+              title={label}
+              style={{ ...arrowStyles, right: 15 }}
+            >
+              {'>'}
+            </button>
+          )
+        }
       >
         {app.demos.map(demo => <Demo wide={wide} demo={demo} />)}
       </Carousel>
-      <div className="carousel-arrows-container">
-        <div className="carousel-arrow-left" onClick={() => changeSlide(-1)}>
-          {'<'}
-        </div>
-        <div className="carousel-arrow-right" onClick={() => changeSlide(1)}>
-          {'>'}
-        </div>
-      </div>
     </div>
   );
 }
 
+// <div className="carousel-arrows-container">
+//   <div className="carousel-arrow-left" onClick={() => changeSlide(-1)}>
+//     {'<'}
+//   </div>
+//   <div className="carousel-arrow-right" onClick={() => changeSlide(1)}>
+//     {'>'}
+//   </div>
+// </div>
 export default Application;
