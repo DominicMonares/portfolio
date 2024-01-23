73ddf6572c843b2c636f3fc5e144039339461178
DominicMonares <dominicsmonares@gmail.com>
2024-01-22 20:54:35 -0800

    refactor rows

--- client/src/Components/Skills/index.jsx
+++ client/src/Components/Skills/index.jsx
@@ -11,25 +11,15 @@ const Skills = ({ wide }) => {
     const allSkills = [];
     for (const category in skills) {
       const skillRows = [];
-      let row = [];
       skills[category].forEach(s => {
-        if (row.length === 6) {
-          skillRows.push(<div className={wide ? 'w_skills' : 'm_skills'}>{row}</div>);
-          row = [];
-        }
-
         const dims = s['dims'][device];
-        row.push(
+        skillRows.push(
           <div className={wide ? 'w_tool' : 'm_tool'}>
             <span className={wide ? 'w_tool_text' : 'm_tool_text'}>{s.title}</span>
             <img src={s.icon} width={dims.width} height={dims.height} />
           </div>
         );
       });
-
-      if (row.length) {
-        skillRows.push(<div className={wide ? 'w_skills' : 'm_skills'}>{row}</div>);
-      }
 
       allSkills.push(
         <div className={wide ? 'w_category_container' : 'm_category_container'}>
