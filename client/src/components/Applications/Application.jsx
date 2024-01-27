2144aa1d96529c61262e6d3c1fd3f171fb57b4ea
DominicMonares <dominicsmonares@gmail.com>
2024-01-27 15:48:40 -0800

    update props and class names

--- client/src/components/Applications/Application.jsx
+++ client/src/components/Applications/Application.jsx
@@ -1,29 +1,12 @@
 import React, { useState } from 'react';
 import { Carousel } from 'react-responsive-carousel';
-
 import Header from '../Shared/Header';
 import SubHeader from '../Shared/SubHeader';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import Demo from './Demo';
 
-const Application = ({ wide, app }) => {
+const Application = ({ swClass, app }) => {
   const [currentSlide, setCurrentSlide] = useState(0);
-  const [autoPlay, setAutoPlay] = useState(true);
-  const mobileDemo = app.demos.every(d => d.dims.web.width < 200);
-
-  const getAppClass = () => {
-    if (wide) {
-      // return mobileDemo ? 'w_mobile_demo_container' : 'w_web_demo_container';
-      return 'w_demo_carousel_container';
-    } else {
-      return 'm_demo_container';
-    }
-  }
-
-  const changeSlide = (direction) => {
-    if (autoPlay) setAutoPlay(false);
-    setCurrentSlide(currentSlide + direction);
-  }
 
   const updateCurrentSlide = (index) => {
     if (currentSlide !== index) setCurrentSlide(index)
@@ -60,19 +43,16 @@ const Application = ({ wide, app }) => {
     right: 15,
   };
 
-  // autoPlay={autoPlay}
-  // interval={6000}
-
   return (
-    <div className={wide ? 'w_app_container' : 'm_app_container'}>
+    <div className={swClass.concat('app-container')}>
       <Header
-        wide={wide}
+        swClass={swClass}
         title={app.title}
         est={app.desc}
         links={app.links}
         techs={app.techs}
       />
-      <SubHeader wide={wide} desc={app.techs} />
+      <SubHeader swClass={swClass} desc={app.techs} />
       <Carousel
         infiniteLoop={true}
         onChange={updateCurrentSlide}
@@ -106,18 +86,10 @@ const Application = ({ wide, app }) => {
           )
         }
       >
-        {app.demos.map(demo => <Demo wide={wide} demo={demo} />)}
+        {app.demos.map(demo => <Demo swClass={swClass} demo={demo} />)}
       </Carousel>
     </div>
   );
 }
 
-// <div className="carousel-arrows-container">
-//   <div className="carousel-arrow-left" onClick={() => changeSlide(-1)}>
-//     {'<'}
-//   </div>
-//   <div className="carousel-arrow-right" onClick={() => changeSlide(1)}>
-//     {'>'}
-//   </div>
-// </div>
 export default Application;
