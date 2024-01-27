7ae5d653685c3e23a229d8c5694e8c456b32669a
DominicMonares <dominicsmonares@gmail.com>
2024-01-26 22:07:18 -0800

    rename dir

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,38 +1,38 @@
 import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import NavBar from './Components/NavBar';
-import Dropdown from './Components/Dropdown';
-import About from './Components/About';
-import Skills from './Components/Skills';
-import Experience from './Components/Experience';
-import Education from './Components/Education';
-import Applications from './Components/Applications';
+import NavBar from './components/NavBar';
+import Dropdown from './components/Dropdown';
+import About from './components/About';
+import Skills from './components/Skills';
+import Experience from './components/Experience';
+import Education from './components/Education';
+import Applications from './components/Applications';
 import './App.css';
 
 const App = () => {
-  const isWide = () =>  window.innerWidth >= 790 ? true : false;
-  const [wide, setWide] = useState(isWide());
+  const isSmallWindow = () =>  window.innerWidth < 790 ? true : false;
+  const [smallWindow, setSmallWindow] = useState(isSmallWindow());
 
   useEffect(() => {
-    const handleWidthChange = () => setWide(isWide());
+    const handleWidthChange = () => setSmallWindow(isSmallWindow());
     window.addEventListener('resize', handleWidthChange);
     return () => window.removeEventListener('resize', handleWidthChange);
   }, []);
 
   return (
-    <div id={wide ? 'w-app' : 'm-app'}>
-      <h1 className={wide ? 'w-title' : 'm-title'}>Dominic Monares</h1>
-      <h3 className={wide ? 'w-subtitle' : 'm-subtitle'}>Full-Stack Software Engineer</h3>
+    <div id={smallWindow ? 'w-app' : 'm-app'}>
+      <h1 className={smallWindow ? 'w-title' : 'm-title'}>Dominic Monares</h1>
+      <h3 className={smallWindow ? 'w-subtitle' : 'm-subtitle'}>Full-Stack Software Engineer</h3>
       <Router>
-        {wide ? <NavBar /> : <Dropdown />}
+        {smallWindow ? <NavBar /> : <Dropdown />}
         <div className="route-container">
           <Routes>
-            <Route path='/apps' element={<Applications wide={wide} />}></Route>
-            <Route path='/about' element={<About wide={wide} />}></Route>
-            <Route path='/skills' element={<Skills wide={wide} />}></Route>
-            <Route path='/experience' element={<Experience wide={wide} />}></Route>
-            <Route path='/education' element={<Education wide={wide} />}></Route>
+            <Route path='/apps' element={<Applications smallWindow={smallWindow} />}></Route>
+            <Route path='/about' element={<About smallWindow={smallWindow} />}></Route>
+            <Route path='/skills' element={<Skills smallWindow={smallWindow} />}></Route>
+            <Route path='/experience' element={<Experience smallWindow={smallWindow} />}></Route>
+            <Route path='/education' element={<Education smallWindow={smallWindow} />}></Route>
           </Routes>
         </div>
       </Router>
