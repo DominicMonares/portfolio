91735531977ec9b9a40d0205f1a1e8b8c6d2e160
akaClay <domakaclay@gmail.com>
2024-01-29 23:51:35 -0800

    add about

--- server/routes.js
+++ server/routes.js
@@ -1,3 +1,4 @@
+const about = require('../data/about.json');
 const education = require('../data/education.json');
 const experience = require('../data/experience.json');
 const navigation = require('../data/navigation.json');
@@ -8,16 +9,20 @@ app.get('/', (req, res) => {
   res.redirect('/applications');
 });
 
+app.get('/data/about', (req, res) => {
+  res.json(education);
+});
+
 app.get('/data/edu', (req, res) => {
   res.json(education);
-})
+});
 
 app.get('/data/exp', (req, res) => {
   res.json(experience);
-})
+});
 
 app.get('/data/nav', (req, res) => {
   res.json(navigation);
-})
+});
 
 module.exports = app;
