40258848b338ee0f01fb203168cbd112f270d4ae
DominicMonares <dominicsmonares@gmail.com>
2024-01-29 23:13:26 -0800

    cleanup

--- client/src/components/Experience/index.jsx
+++ client/src/components/Experience/index.jsx
@@ -1,10 +1,21 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import regeneratorRuntime from 'regenerator-runtime';
 import Job from './Job';
-import experience from '../../../../data/experience.js';
 import './Experience.css';
 
 const Experience = ({ swClass }) => {
+  const [experience, setExperience] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => await fetch('/data/exp');
+    fetchData()
+      .then(async res => {
+        const data = await res.json();
+        setExperience(data);
+      })
+      .catch(err => { throw err })
+  }, []);
+
   return (
     <div className={swClass.concat('experience-container')}>
       {experience.map(job => <Job swClass={swClass} job={job} />)}
--- server/routes.js
+++ server/routes.js
@@ -1,3 +1,4 @@
+const experience = require('../data/experience.json');
 const navigation = require('../data/navigation.json');
 
 const app = require('express').Router();
@@ -5,6 +6,10 @@ const app = require('express').Router();
 app.get('/', (req, res) => {
   res.redirect('/applications');
 });
+
+app.get('/data/exp', (req, res) => {
+  res.json(experience);
+})
 
 app.get('/data/nav', (req, res) => {
   res.json(navigation);
