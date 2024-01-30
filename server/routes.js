030ba747943a75c49c3fb9a1533655bf6416693e
DominicMonares <dominicsmonares@gmail.com>
2024-01-29 21:59:23 -0800

    add data nave route

--- server/routes.js
+++ server/routes.js
@@ -1,7 +1,13 @@
+const navigation = require('../data/navigation.json');
+
 const app = require('express').Router();
 
 app.get('/', (req, res) => {
   res.redirect('/apps');
 });
+
+app.get('/data/nav', (req, res) => {
+  res.json(navigation);
+})
 
 module.exports = app;
