f6ca1ca92b439a5498269a3e745b42829f8a759f
akaClay <domakaclay@gmail.com>
2024-01-31 23:26:16 -0800

    fix dims

--- client/src/components/Skills/index.jsx
+++ client/src/components/Skills/index.jsx
@@ -1,21 +1,37 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import regeneratorRuntime from 'regenerator-runtime';
 import Icons from '../Shared/Icons';
-import skills from '../../../../data/skills';
+import images from '../../images';
 import './Skills.css';
 
 const Skills = ({ swClass }) => {
+  const [skills, setSkills] = useState({});
+
+  useEffect(() => {
+    const fetchData = async () => await fetch('/data/skills');
+    fetchData()
+      .then(async res => {
+        const data = await res.json();
+        setSkills(data);
+      })
+      .catch(err => { throw err })
+  }, []);
+
   const sortSkills = () => {
     const allSkills = [];
     for (const category in skills) {
       const skillRows = [];
       skills[category].forEach(s => {
-        const dims = s['dims'][swClass ? 'web' : 'mobile'];
+        const dims = s['dims'][swClass ? 'mobile' : 'web'];
         skillRows.push(
           <div className={swClass.concat('tool')}>
             <span className={swClass.concat('tool-text')}>{s.title}</span>
             <div className="tool-img-container">
-              <img src={s.icon} width={dims.width} height={dims.height} />
+              <img
+                src={images['skills'][s.icon]}
+                width={dims.width}
+                height={dims.height}
+              />
             </div>
           </div>
         );
@@ -41,7 +57,7 @@ const Skills = ({ swClass }) => {
       <div className="skills">
         {sortSkills()}
       </div>
-      <Icons swClass={swClass} page="Skills" />
+      <Icons swClass={swClass} page="skills" />
     </div>
   );
 }
