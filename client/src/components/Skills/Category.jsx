3b72395588cfa6c3ac09e60ec5d1987a6f1e8a59
DominicMonares <dominicsmonares@gmail.com>
2024-02-01 21:09:18 -0800

    add list

--- client/src/components/Skills/Category.jsx
+++ client/src/components/Skills/Category.jsx
@@ -4,23 +4,23 @@ import images from '../../images';
 const Category = ({ swClass, category, skills }) => {
   return (
     <div className={swClass.concat('category-container')}>
-      <span className={swClass.concat('category-name')}>
+      <div className={swClass.concat('category-name')}>
         {category}
-      </span>
-      <div className="skills">
+      </div>
+      <ul className="skills">
         {skills[category].map(s => (
-          <div className={swClass.concat('skill')}>
+          <li className={swClass.concat('skill')}>
             <span className={swClass.concat('skill-text')}>{s.title}</span>
-            <div className="skill-img-container">
+            <span className="skill-img-container">
               <img
                 src={images['skills'][s.icon]}
                 width={s.dims.width}
                 height={s.dims.height}
               />
-            </div>
-          </div>
+            </span>
+          </li>
         ))}
-      </div>
+      </ul>
     </div>
   );
 }
