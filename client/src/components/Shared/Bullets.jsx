3edc0522ec99d4b1342d981e2870a14df7819e55
DominicMonares <dominicsmonares@gmail.com>
2024-02-01 19:28:07 -0800

    cleanup

--- client/src/components/Shared/Bullets.jsx
+++ client/src/components/Shared/Bullets.jsx
@@ -1,32 +1,19 @@
 import React from 'react';
-
 import './Shared.css';
 
 const Bullets = ({ swClass, bullets }) => {
-  const bold = (bullet) => {
-    const splitBullet = bullet.split('<b>');
-    let odd = true;
-    return splitBullet.map(b => {
-      if (odd) {
-        odd = false;
-        return <span key={b}>{b}</span>
-      }
-
-      odd = true;
-      return <b key={b}>{b}</b>
-    });
-  }
-
   return (
     <div className={swClass.concat('bullet-container')}>
-      {bullets.map(bullet => {
-        return (
-          <div className={swClass.concat('bullet')}>
-            •
-            <p className={swClass.concat('bullet-text')}>{bold(bullet)}</p>
-          </div>
-        );
-      })}
+      {bullets.map(bullet => (
+        <div className={swClass.concat('bullet')}>
+          •
+          <p className={swClass.concat('bullet-text')}>
+            {bullet.split('<b>').map((b, i) => (
+              i % 2 === 1 ? <b key={b}>{b}</b> : <span key={b}>{b}</span>
+            ))}
+          </p>
+        </div>
+      ))}
     </div>
   );
 }
