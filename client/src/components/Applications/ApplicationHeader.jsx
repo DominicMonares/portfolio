053a4b74859e120b008ff36f033593b71af7faaa
DominicMonares <dominicsmonares@gmail.com>
2024-02-01 18:53:06 -0800

    init

---
+++ client/src/components/Applications/ApplicationHeader.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const ApplicationHeader = ({ swClass, title, desc, links }) => {
+  return (
+    <div className={swClass.concat('application-header')}>
+      <span className={swClass.concat('application-header-title')}>
+        {title}
+      </span>
+      &nbsp;|&nbsp;
+      <span>{desc}</span>
+      &nbsp;|&nbsp;
+      {links.map((a, i) => (
+        <span>
+          <a
+            className={swClass.concat('application-header-link')}
+            href={a.link}
+            target='_blank'
+            key={a.link}
+          >
+            {a.label}
+          </a>
+          {i < links.length - 1 ? (
+            <span>&nbsp;|&nbsp;</span>
+          ) : (
+            <span></span>
+          )}
+        </span>
+      ))}
+    </div>
+  );
+}
+
+export default ApplicationHeader;
