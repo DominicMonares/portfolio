7079efa66cbb95a2df956dca969f5cb29b85737c
DominicMonares <dominicsmonares@gmail.com>
2024-02-01 21:06:38 -0800

    refactor

--- client/src/components/Skills/index.jsx
+++ client/src/components/Skills/index.jsx
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import regeneratorRuntime from 'regenerator-runtime';
+import Category from './Category';
 import Icons from '../Shared/Icons';
-import images from '../../images';
 import './Skills.css';
 
 const Skills = ({ swClass }) => {
+  // Fetch skill data
   const [skills, setSkills] = useState({});
-
   useEffect(() => {
     const fetchData = async () => await fetch('/data/skills');
     fetchData()
@@ -17,45 +17,16 @@ const Skills = ({ swClass }) => {
       .catch(err => { throw err })
   }, []);
 
-  const sortSkills = () => {
-    const allSkills = [];
-    for (const category in skills) {
-      const skillRows = [];
-      skills[category].forEach(s => {
-        const dims = s['dims'][swClass ? 'mobile' : 'web'];
-        skillRows.push(
-          <div className={swClass.concat('tool')}>
-            <span className={swClass.concat('tool-text')}>{s.title}</span>
-            <div className="tool-img-container">
-              <img
-                src={images['skills'][s.icon]}
-                width={dims.width}
-                height={dims.height}
-              />
-            </div>
-          </div>
-        );
-      });
-
-      allSkills.push(
-        <div className={swClass.concat('category-container')}>
-          <span className={swClass.concat('category-name')}>
-            {category}
-          </span>
-          <div className="tools">
-            {skillRows}
-          </div>
-        </div>
-      );
-    }
-
-    return allSkills;
-  }
-
   return (
     <div className={swClass.concat('skills-container')}>
-      <div className="skills">
-        {sortSkills()}
+      <div className="categories-container">
+        {Object.keys(skills).map(category => (
+          <Category
+            swClass={swClass}
+            category={category}
+            skills={skills}
+          />
+        ))}
       </div>
       <Icons swClass={swClass} page="skills" />
     </div>
