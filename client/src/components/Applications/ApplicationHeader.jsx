7e1363bb96aed99bde555c91f1f5988fbd79a1f9
DominicMonares <dominicsmonares@gmail.com>
2024-02-04 14:48:33 -0800

    refactor app header

--- client/src/components/Applications/ApplicationHeader.jsx
+++ client/src/components/Applications/ApplicationHeader.jsx
@@ -4,29 +4,32 @@ import './Applications.css';
 const ApplicationHeader = ({ swClass, title, desc, links }) => {
   return (
     <div className={swClass.concat('application-header')}>
-      <span className={swClass.concat('application-header-title')}>
-        {title}
-      </span>
-      <span>&nbsp;|&nbsp;</span>
-      <span>{desc}</span>
-      <span>&nbsp;|&nbsp;</span>
-      {links.map((a, i) => (
-        <span>
-          <a
-            className={swClass.concat('application-header-link')}
-            href={a.link}
-            target="_blank"
-            key={a.link}
-          >
-            {a.label}
-          </a>
-          {i < links.length - 1 ? (
-            <span>&nbsp;|&nbsp;</span>
-          ) : (
-            <span />
-          )}
+      <div className={swClass.concat('application-header-top-container')}>
+        <span className={swClass.concat('application-header-title')}>
+          {title}
         </span>
-      ))}
+        <span className={swClass.concat('application-header-links')}>
+          {links.map((a, i) => (
+            <span>
+              <a
+                className={swClass.concat('application-header-link')}
+                href={a.link}
+                target="_blank"
+                key={a.link}
+              >
+                {a.label}
+              </a>
+              {i < links.length - 1 ? (
+                <span>&nbsp;|&nbsp;</span>
+              ) : (
+                <span />
+              )}
+            </span>
+          ))}
+
+        </span>
+      </div>
+      <div>{desc}</div>
     </div>
   );
 }
